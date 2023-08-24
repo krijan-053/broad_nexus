378388856c2031240b9a99391f23cf1bf819a4dd
krijan_053 <120587254+krijan-053@users.noreply.github.com>
2023-08-24 15:39:41 +0545

    Update views.py
    fourth

--- api/views.py
+++ api/views.py
@@ -2,10 +2,13 @@ from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
+from rest_framework.generics import ListAPIView
 
 from core.models import Category, Job, JobApplication, Contact
+from account.models import User, UserProfile
 
-from .serializers import CategorySerializers, JobModelSerializer, ContactSerializer
+from .serializers import CategorySerializers, JobModelSerializer, ContactSerializer, JobApplicationSerializer, \
+    UserProfileSerializer, UserSerializer
 
 
 # Create your views here.
@@ -20,14 +23,6 @@ class JobViewSet(ModelViewSet):
     serializer_class = JobModelSerializer
 
 
-class ContactCreateView(CreateAPIView):
-    def post(self, request, *args, **kwargs):
-        serializer = ContactSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        validated_data = seralizer.validated_data
-        Contact.objects.create(**validated_data)
-        return Response(serializer.data)
-
 class ContactListCreateView(APIView):
 
     def get(self, *args, **kwargs):
@@ -41,3 +36,28 @@ class ContactListCreateView(APIView):
         validated_data = serializer.validated_data
         Contact.objects.create(**validated_data)
         return Response(serializer.data)
+
+
+class JobApplicationListCreateView(APIView):
+
+    def get(self, *args, **kwargs):
+        application = JobApplication.objects.all()
+        serializer = JobApplicationSerializer(application, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        serializer = JobApplicationSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        validated_data = serializer.validated_data
+        JobApplication.objects.create(**validated_data)
+        return Response(serializer.data)
+
+
+class UserView(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserProfileView(ModelViewSet):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
