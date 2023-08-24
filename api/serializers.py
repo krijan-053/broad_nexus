9fef931db6a990f27f62d3987899550d63e6ffed
krijan_053 <120587254+krijan-053@users.noreply.github.com>
2023-08-24 15:40:21 +0545

    Update serializers.py
    fourth

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from core.models import Category, Job, JobApplication, Contact
+from account.models import User, UserProfile
 
 
 # Create your serializers here.
@@ -20,3 +21,21 @@ class ContactSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contact
         fields = ["name", "email", "phone_number", "message"]
+
+
+class JobApplicationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = JobApplication
+        fields = ['job', 'user', 'interview_date', 'status']
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['email', 'account_activated']
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = ['user', 'profile_picture', 'resume', 'address', 'phone_number', 'about_me']
