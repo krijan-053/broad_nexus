128373947b8f842f26afe384cc9560505e1fa326
krijan_053 <120587254+krijan-053@users.noreply.github.com>
2023-08-24 15:42:15 +0545

    Update urls.py
    fourth

--- api/urls.py
+++ api/urls.py
@@ -2,14 +2,18 @@ from django.urls import path
 
 from rest_framework.routers import DefaultRouter
 
-from .views import CategoryViewSet, JobViewSet, ContactCreateView , ContactListCreateView
+from .views import CategoryViewSet, JobViewSet, ContactListCreateView, JobApplicationListCreateView,\
+    UserView, UserProfileView
 
 router = DefaultRouter()
 router.register('category', CategoryViewSet, basename="category")
 router.register('job', JobViewSet, basename="job")
-# router.register('c-app', ContactListCreateView, basename="c-app")
+router.register('user-profile', UserProfileView, basename="user_profile")
+router.register('user',UserView,basename='user')
 
 
 urlpatterns = [
-                  path("contact-api/",ContactListCreateView.as_view())
+                    path("contact-api/",ContactListCreateView.as_view()),
+                    path("job-application/",JobApplicationListCreateView.as_view()),
+
               ] + router.urls
